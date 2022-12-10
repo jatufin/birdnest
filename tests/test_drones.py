1dff8ba8dd3dc4fe4834a42459baf68d26be735b
Janne Tuukkanen <jatufin@gmail.com>
2022-12-10 22:13:53 +0200

    Update tests

--- tests/test_drones.py
+++ tests/test_drones.py
@@ -1,26 +1,47 @@
 import unittest
-
+from unittest.mock import MagicMock
 from drones import Drones
+
+from datetime import datetime
 
 
 class TestDrones(unittest.TestCase):
     def setUp(self):
-        self.drone = Drones("http://service/drones",
-                            "http://service/pilots",
-                            250000,
-                            250000,
-                            radius=100,
-                            persist_time=600,
-                            time_format="%Y-%m-%dT%H:%M:%S.%fZ")
+        self.service = MagicMock()
+        self.service.get_drones.return_value = \
+            {"serialXXX": {"timestamp": datetime.utcnow(),
+                           "positionX": 175000.0,
+                           "positionY": 250000.0}}
+        self.service.get_pilot.return_value = \
+            {"distance": 10000,
+             "firstName": "Jean",
+             "lastName": "Doe",
+             "email": "e@mail"}
+        
+        self.nest = MagicMock()
+        self.nest.too_close.return_value = 50000
+
+        self.drones = Drones(self.service,
+                             self.nest,
+                             10)
+        self.drones.drones = \
+            {"serialXXX": {"timestamp": 1,
+                           "distance": 10000}}
         
     def test_drone_can_be_created(self):
-        self.assertEqual(self.drone.drones_url, "http://service/drones")
+        self.assertEqual(self.drones.persist_time, 10)
 
-    def test_too_close_returns_correct_values(self):
-        self.assertEqual(self.drone._too_close(151000, 250000), 99000.0)
-        self.assertEqual(self.drone._too_close(150000, 250000), None)
-        self.assertEqual(int(self.drone._too_close(225000, 225000)), 35355)        
+    def test_update_offending_drones_calls_service(self):
+        self.drones.update_offending_drones()
+        
+        self.service.get_drones.assert_called_once()
+
+    def test_get_offending_pilots_calls_service(self):
+        self.drones.get_offending_pilots()
+
+        self.service.get_pilot.assert_called_once()
 
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
